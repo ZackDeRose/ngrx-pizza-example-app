443c2666276a10823eb367b4f0c04444c0d2c049
Zack DeRose <zack@nrwl.io>
2021-02-02 23:59:43 -0700

    starting point

--- apps/pizza/src/app/app.module.ts
+++ apps/pizza/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pizza/pizza.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PizzaComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
